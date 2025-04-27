4580ed123e145024bc1a6c4e274b8740300d78ed
Mike <mikemabunda478@gmail.com>
2025-04-27 17:14:19 +0200

    Create MeuparPerfeitoApplication.java

---
+++ MeuparPerfeitoApplication.java
@@ -0,0 +1,11 @@
+package com.meupar.perfeito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuparPerfeitoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MeuparPerfeitoApplication.class, args);
+    }
+}
